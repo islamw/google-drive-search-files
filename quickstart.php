c5aba6b41d922871a1f4647143a24ee993300672
islamw <islam.khandurdyiev@gmail.com>
2017-09-14 23:55:37 +0300

    init

---
+++ quickstart.php
@@ -0,0 +1,133 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+const APPLICATION_NAME = 'Drive API PHP Quickstart';
+
+const CREDENTIALS_PATH = '~/.credentials/drive-php-quickstart.json';
+
+const CLIENT_SECRET_PATH = __DIR__.'/client_secret.json';
+
+const EXTENSIONS = ['pdf', 'jpg', 'jpeg'];
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/drive-php-quickstart.json
+
+define(
+    'SCOPES',
+    implode(
+        ' ',
+        [
+            Google_Service_Drive::DRIVE
+        ]
+    )
+);
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ *
+ * @return \Google_Client the authorized client object
+ * @throws \Google_Exception
+ * @throws \InvalidArgumentException
+ * @throws \LogicException
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName(APPLICATION_NAME);
+    $client->setScopes(SCOPES);
+    $client->setAuthConfig(CLIENT_SECRET_PATH);
+    $client->setAccessType('offline');
+    
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
+    if (file_exists($credentialsPath)) {
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
+        
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+        
+        // Store the credentials to disk.
+        if (!file_exists(dirname($credentialsPath))) {
+            mkdir(dirname($credentialsPath), 0700, true);
+        }
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        printf("Credentials saved to %s\n", $credentialsPath);
+    }
+    $client->setAccessToken($accessToken);
+    
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    
+    return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ *
+ * @param string $path the path to expand.
+ *
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path)
+{
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+        $homeDirectory = getenv('HOMEDRIVE').getenv('HOMEPATH');
+    }
+    
+    return str_replace('~', realpath($homeDirectory), $path);
+}
+
+/**
+ * @param \Google_Service_Drive $service
+ * @param $argv
+ *
+ * @return bool|string
+ */
+function getFileLink(Google_Service_Drive $service, $argv)
+{
+    if (count($argv) < 2) {
+        return "Enter name of file!";
+    }
+    
+    $extensionsKeys   = array_keys(EXTENSIONS);
+    $lastExtensionKey = end($extensionsKeys);
+    
+    foreach (EXTENSIONS as $key => $EXTENSION) {
+        $name   = "name='".$argv[1].".".$EXTENSION."'";
+        $result = $service->files->listFiles(['q' => $name, 'fields' => 'files(webViewLink)']);
+        $files  = $result->getFiles();
+        if (empty($files)) {
+            if ($lastExtensionKey == $key) {
+                return 'This file doesn\'t exists';
+            }
+            continue;
+        } else {
+            return $files[0]->webViewLink;
+        }
+    }
+    
+    return false;
+}
+
+
+// Get the API client and construct the service object.
+$client  = getClient();
+$service = new Google_Service_Drive($client);
+
+$result = getFileLink($service, $argv);
+
+print $result;
