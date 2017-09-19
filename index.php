2a857c38f4d735f77b7da7f7536c8a71ce0a5d1b
xor <islam.khandurdyiev@gmail.com>
2017-09-19 13:39:28 +0300

    init

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 require_once __DIR__. '/kernel.php';
 
+$refs = ["CV", "CV", "CV"];
+
 $client = getClient();
 
 if ( isExistsToken() ) {
 	$client->setAccessToken( $_SESSION['access_token'] );
-	echo getFileLink( new Google_Service_Drive( $client ), "CV" );
+	$service = new Google_Service_Drive($client);
+	foreach($refs as $ref) {
+		$result = getFileLink($service, "$ref");
+		$fp = fopen("PDFOF$ref.json", 'w');
+		fwrite($fp, json_encode($result));
+		fclose($fp);
+	}
 	
 	return true;
 }
